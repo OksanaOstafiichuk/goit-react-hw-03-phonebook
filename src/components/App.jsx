5de514c00629e9aa8f55c11996b7160175dfea02
OksanaOstafiichuk <Ostafiichukk@gmail.com>
2022-07-15 12:41:06 +0300

    save phonebook on localStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,16 +6,30 @@ import { ContactForm } from './ContactForm/ContactForm';
 import { Filter } from './Filter/Filter';
 import { PhoneBook, Title, Contacts } from './App.styled';
 
+const LOCALSTORAGE_KEY = 'contacts';
+
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
+
+  componentDidMount() {
+    const contactList = localStorage.getItem(LOCALSTORAGE_KEY);
+    const parsedContactList = JSON.parse(contactList);
+
+    if (parsedContactList) {
+      this.setState({ contacts: parsedContactList });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const nextState = this.state.contacts;
+
+    if (prevState.contacts !== nextState) {
+      localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(nextState));
+    }
+  }
 
   addContact = ({ name, number }) => {
     const { contacts } = this.state;
